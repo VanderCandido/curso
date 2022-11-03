f3f88430e350e500ec2070d3b7b49f42864c1274
VanderCandido <114668363+VanderCandido@users.noreply.github.com>
2022-11-03 16:11:03 -0300

    Create JS_4,3w.js

---
+++ js/JS_4,3w.js
@@ -0,0 +1,25 @@
+const pessoa = ["v", "a", "n", "d", "e", "r"];
+
+const trabalho = {
+    Nome: 'montador',
+    Tipo: 'montar',
+    horário: '8 horas'
+}
+
+function curso(){
+    console.log("\nQue curso estou fazendo agora?");
+    for (const e of curso) {
+        console.log(e);
+    }
+}
+
+function trablahdor(){
+    console.log("\nDados do trabalhador:\n")
+    for (const index in trabalho){
+        console.log(`${index} : ${trablaho[index]}`)
+    }
+}
+
+curso()
+
+trabalhador()
